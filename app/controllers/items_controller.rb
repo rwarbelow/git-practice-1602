8fb378b6251156a41143d616c07d7bf2619d2aab
Rachel Warbelow <rachelwarbelow@gmail.com>
2016-04-14 11:21:50 -0600

    stuff

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,5 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @items = Item.all
-  end
 
-  def show
-  end
-
-  private
-    def set_item
-      @item = Item.find(params[:id])
-    end
-
-    def item_params
-      params.require(:item).permit(:name, :description, :price)
-    end
 end
