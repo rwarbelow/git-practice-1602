0752270b5748a498e02a2afc7e743ec416c314e9
Matt Pindell <pindell.m@gmail.com>
2016-04-14 11:34:48 -0600

    Update comments on ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,12 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /items
-  # GET /items.json
   def index
     @items = Item.all
   end
 
-  # GET /items/1
+  # my name is Matt and I totes luv making comments
+
   # GET /items/1.json
   def show
   end
